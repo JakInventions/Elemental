8e229318d40a741824c7ae441910ec93d2ee1c2d
Jak <jaksawesome10@github.com>
2019-03-21 22:16:06 -0400

    stuff

--- NewElemental/platforms/android/app/src/main/assets/www/assets/js/main.js
+++ NewElemental/platforms/android/app/src/main/assets/www/assets/js/main.js
@@ -1,9 +1,37 @@
 
 window.addEventListener("load", function(){
 	
+	var settingsButtonRotation = 0;
+	
 	$(document).ready(function(){
 		
 		$("body").fadeIn(500);
+		
+	});
+	
+	document.getElementById("settingsButton").addEventListener("click", function(){
+		
+		$(document).ready(function(){
+			
+			settingsButtonRotation += 180;
+			
+			document.getElementById("settingsButton").setAttribute('style','transform:rotate(' + settingsButtonRotation + 'deg)');
+			
+			if((settingsButtonRotation / 180) % 2 == 1){
+				
+				document.getElementById("settingsButton").style.marginTop = "-15px";
+				
+			}
+			
+			else{
+				
+				document.getElementById("settingsButton").style.marginTop = "-16px";
+				
+			}
+			
+			$(".settingsView").slideToggle(500);
+			
+		});
 		
 	});
 	
