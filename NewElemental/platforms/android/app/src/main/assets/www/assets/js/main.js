b6fb5063b62c082558f6f3dc6e058d56c4c4d7ac
Jak <jaksawesome10@github.com>
2019-03-22 21:18:03 -0400

    some work

--- NewElemental/platforms/android/app/src/main/assets/www/assets/js/main.js
+++ NewElemental/platforms/android/app/src/main/assets/www/assets/js/main.js
@@ -1,7 +1,33 @@
 
+var settingsButtonRotation = 0;
+
+var restartButton1Rotation = 0;
+
+var restartButton2Rotation = 0;
+
+if(localStorage.getItem("player1Score") == null){
+	
+	localStorage.setItem("player1Score", "20");
+	
+}
+
+if(localStorage.getItem("player2Score") == null){
+	
+	localStorage.setItem("player2Score", "20");
+	
+}
+
+function refreshScore(){
+	
+	document.getElementById("player1Score").innerHTML = localStorage.getItem("player1Score");
+	
+	document.getElementById("player2Score").innerHTML = localStorage.getItem("player2Score");
+	
+}
+
 window.addEventListener("load", function(){
 	
-	var settingsButtonRotation = 0;
+	refreshScore();
 	
 	$(document).ready(function(){
 		
@@ -29,11 +55,80 @@ window.addEventListener("load", function(){
 				
 			}
 			
-			$(".settingsView").slideToggle(500);
+			slideSettings = false;
+			
+			for(var i = 0; i < document.getElementsByClassName("settingsView").length; i++){
+				
+				if(document.getElementsByClassName("settingsView")[i].offsetHeight == "60" || document.getElementsByClassName("settingsView")[i].offsetHeight == "0"){
+					
+					slideSettings = true;
+					
+				}
+				
+				
+			}
+			
+			if(slideSettings)$(".settingsView").slideToggle(500);
 			
 		});
 		
 	});
 	
 });
+
+function subtractPlayer2(){
+	
+	localStorage.setItem("player2Score", parseInt(localStorage.getItem("player2Score")) - 1);
+	
+	refreshScore();
+	
+}
+
+function addPlayer2(){
+	
+	localStorage.setItem("player2Score", parseInt(localStorage.getItem("player2Score")) + 1);
+	
+	refreshScore();
+	
+}
+
+function subtractPlayer1(){
+	
+	localStorage.setItem("player1Score", parseInt(localStorage.getItem("player1Score")) - 1);
+	
+	refreshScore();
+	
+}
+
+function addPlayer1(){
+	
+	localStorage.setItem("player1Score", parseInt(localStorage.getItem("player1Score")) + 1);
+	
+	refreshScore();
+	
+}
+
+function restartPlayer1(ele){
+	
+	restartButton1Rotation -= 360;
+	
+	ele.setAttribute('style','transform:rotate(' + restartButton1Rotation + 'deg)');
+	
+	localStorage.setItem("player1Score", "20");
+	
+	refreshScore();
+	
+}
+
+function restartPlayer2(ele){
+	
+	restartButton2Rotation -= 360;
+	
+	ele.setAttribute('style','transform:rotate(' + restartButton2Rotation + 'deg)');
+	
+	localStorage.setItem("player2Score", "20");
+	
+	refreshScore();
+	
+}
 
